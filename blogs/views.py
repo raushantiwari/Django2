8457c7bbaba7009dcb23eaa39aa070373e7a0eeb
Raushan Tiwari <raushan.tiwari@tothenew.com>
2019-02-24 15:30:33 +0530

    create blog done

--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .admin import Blogs
 from django.contrib.auth.decorators import login_required
 from blogs.forms import BlogsForm
-
+import datetime
 
 # Create your views here.
 
@@ -18,5 +18,17 @@ def detail(request, blog_id):
 
 @login_required(login_url='/account/login/')
 def create_blog(request):
-    form = BlogsForm()
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['author'] and request.FILES['image'] and request.POST['body']:
+            blog = Blogs()
+            blog.title = request.POST['title']
+            blog.body = request.POST['body']
+            blog.image = request.FILES['image']
+            # blog.published = request.POST['published']
+            blog.author = request.POST['author']
+            blog.pub_date = datetime.datetime.now()
+            blog.save()
+            return redirect('/blog/' + str(blog.id))
+    else:
+        form = BlogsForm()
     return render(request, 'blogs/create-blog.html', {'form': form})
